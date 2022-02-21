aced9a54a1a04c0c3c39a67a7f2fb5fe86187daf
congadautien123 <76635754+congadautien123@users.noreply.github.com>
2022-02-21 10:43:09 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"ex4/db"
+	"fmt"
 	"log"
 	"strconv"
 	"sync"
@@ -25,36 +26,19 @@ type Point struct {
 	MaxPoints int64
 }
 
-func insertUser(engine *xorm.Engine, id string, name string, dob int64) error {
-	user := Users{
-		ID: id,
-		Name: name,
-		Birth: dob,
-		Created: time.Now().UnixMilli(),
-		UpdatedAt: time.Now().UnixMilli(),
-	}
+func insertUser(engine *xorm.Engine, user interface{}, poi interface{}) error {
 	err := db.InsertTable(engine,"users",user)
 	if err != nil {
 		return err
-	} else {
-		poi := Point{
-			UserId: id,
-			Points: 10,
-			MaxPoints: 10,
-		}
-		err := db.InsertTable(engine,"point",poi)
-		if err != nil {
-			return err
-		}
+	}
+	err1 := db.InsertTable(engine,"point",poi)
+	if err1 != nil {
+		return err1
 	}
 	return nil
 }
 
-func updateUser(engine *xorm.Engine, id string) error {
-	now := time.Now().UnixMilli()
-	user := Users{
-		UpdatedAt: now,
-	}
+func updateUser(engine *xorm.Engine, user interface{}, id string) error {
 	err := db.UpdateTable(engine, "users", user, id)
 	if err != nil {
 		return err
@@ -88,40 +72,59 @@ func readPoint(engine *xorm.Engine, id string) *Point {
 	return p
 }
 
+func ListUser(engine *xorm.Engine) {
+	u := []Users{}
+	err := engine.Find(&u)
+	if err != nil {
+		log.Println(err)
+	}
+	us:= Users{}
+	count,err := engine.Count(&us)
+	if err != nil {
+		log.Println(err)
+	}
+	for i := 0; i < int(count); i++ {
+		fmt.Printf("User%v: id: %v, Name: %v, Birth: %v, Created: %v, Updated_at: %v\n", i, u[i].ID, u[i].Name,u[i].Birth, u[i].Created, u[i].UpdatedAt)
+	}
+}
+
 func updateBirth(engine *xorm.Engine, id string, dob int64) error {
 	session := engine.NewSession()
 	defer session.Close()
-	if err := session.Begin(); err != nil {
-		return err
+	er := session.Begin()
+	if er != nil {
+		session.Rollback()
+		return er
 	}
 	user := Users{
 		Birth: dob,
 		UpdatedAt: time.Now().UnixMilli(),
 	}
-	err := db.UpdateTable(engine,"users",user,id)
+	_,err := session.Table("users").ID(id).Update(&user)
 	if err != nil {
+		session.Rollback()
 		return err
-	} else {
-		row := readPoint(engine,id)
-		uppoint := Point{
-			Points: row.Points + 10,
-		}
-		_, err := engine.Table("point").Where("user_id = ?",id).Update(&uppoint)
-		if err != nil {
-			return err
-		} else {
-			row1 := readUser(engine,id)
-			upuser := Users{
-				Name: row1.Name + strconv.FormatInt(row1.UpdatedAt,10),
-				UpdatedAt: time.Now().UnixMilli(),
-			}
-			err := db.UpdateTable(engine, "users", upuser, id)
-			if err != nil {
-				return err
-			}
-		}
 	}
-	err = session.Rollback()
+	row := readPoint(engine,id)
+	uppoint := Point{
+		Points: row.Points + int64(10),
+	}
+	_, err1 := session.Table("point").Where("user_id = ?",id).Update(&uppoint)
+	if err1 != nil {
+		session.Rollback()
+		return err1
+	}
+	row1 := readUser(engine,id)
+	upuser := Users{
+		Name: row1.Name + strconv.FormatInt(row1.UpdatedAt,10),
+		UpdatedAt: time.Now().UnixMilli(),
+	}
+	err2 := db.UpdateTable(engine, "users", upuser, id)
+	if err != nil {
+		session.Rollback()
+		return err2
+	}
+	err = session.Commit()
 	if err != nil {
 		return err
 	}
@@ -131,8 +134,18 @@ func updateBirth(engine *xorm.Engine, id string, dob int64) error {
 func insert100(engine *xorm.Engine) error {
 	for i := 0; i < 100; i++ {
 		id := "a" + strconv.Itoa(i)
-		name := "trung" + strconv.Itoa(i)
-		insertUser(engine, id, name, 1234)
+		user := Users{
+			ID:  id,
+			Name: "trung" + strconv.Itoa(i),
+			Created: time.Now().UnixMilli(),
+			UpdatedAt: time.Now().UnixMilli(),
+		}
+		poi := Point{
+			UserId: id,
+			Points: 10,
+			MaxPoints: 10,
+		}
+		insertUser(engine, user, poi)
 	}
 	return nil
 }
@@ -143,7 +156,7 @@ func delete100(engine *xorm.Engine){
 		users := Users{}
 		point := Point{}
 		err := db.DeleteTable(engine, users, id)
-		err1 := db.DeleteTable(engine, point, id)
+		err1 := db.DeleteTable1(engine, point, id)
 		if err != nil || err1 != nil {
 			log.Println(err, err1)
 		}
@@ -158,7 +171,7 @@ func scantableuser(engine *xorm.Engine) {
 	id1 := make(chan string,3)
 	count,err := engine.Count(&u)
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 	for i := 0; i < int(count); i++ {
 		wg.Add(1)
@@ -167,51 +180,83 @@ func scantableuser(engine *xorm.Engine) {
 		us := readUser(engine, id)
 		name <- us.Name
 		id1 <- us.ID
-		go log.Printf("$(%v) - $(%v) - $(%v)", i, <- id1, <-name)
+		go worker(i,name, id1)
 	}
 	close(name)
 	close(id1)  
 }
 
+func worker(i int,id1 <- chan string, name <- chan string)  {
+	log.Printf("%v - %v - %v", i, <- id1, <-name)
+}
+
 func main(){
-	engine := db.Connect()
+	engine, err := db.Connect()
+	if err != nil {
+		log.Println(err)
+	}
 	defer engine.Close()
-	// engine.ShowSQL(true)
+	engine.ShowSQL(true)
 	//ex1: 
 
 	// 1. Viết hàm: Chỉ tạo db, và tạo model(struct) ánh xạ struct thành table (CreateTable, Sync2)
 
-	// table.CreateTable(engine, Users{})
-	// table.CreateTable(engine, Point{})
+	// err1 := db.CreateTable(engine, Users{})
+	// err1 := db.CreateTable(engine, Point{})
 	
-	// table.DropTable(engine,Users{})
-	// table.DropTable(engine,Point{})
+	//  err1 := db.DropTable(engine,Users{})
+	//  err1 := db.DropTable(engine,Point{})
+	// if err1 != nil {
+	// 	log.Println(err1)
+	// }
 
 	//2. Viết hàm: insert và update user, viết hàm list user hoặc đọc user theo id(4 hàm)
 	//3. Viết hàm: sau khi tạo user thì insert user_id vào user_point với số điểm 10.
 
-	// err := insertUser(engine, "s3", "minh", 1234)
-	// if err != nil {
-	// 	log.Println(err)
+		//insert
+	// id := "d1"
+	// user := Users{
+	// 	ID: id,
+	// 	Name: "Ha",
+	// 	Birth: 1234,
+	// 	Created: time.Now().UnixMilli(),
+	// 	UpdatedAt: time.Now().UnixMilli(),
+	// }
+	// poi := Point{
+	// 	UserId: id,
+	// 	Points: 10,
+	// 	MaxPoints: 10,
 	// }
 
-	// updateUser(engine, "s2")
+	// insertUser(engine, user, poi)
 
-	// db.ListTable(engine,"users")
+		//Update
+	// id:= "s2"
+	// user := Users{
+	// 	ID: id,
+	// 	Name: "aaa",
+	// 	UpdatedAt: time.Now().UnixMilli(),
+	// }
+	// updateUser(engine, user, id)
 
+		//List
+	// ListUser(engine)
+
+		//Read
 	// u := readUser(engine, "a10")
 	// log.Printf("User: \nid: %v\nName: %v\nBirth: %v\n ",u.ID,u.Name,u.Birth)
 
 	//ex2: tạo 1 transaction khi update birth thành công thì cộng 10 điểm vào point sau đó sửa lại name thành $name + "updated " nếu 1 quá trình fail thì rollback, xong commit (CreateSesson)
 
-	// err := updateBirth(engine, "s2", 9999)
-	// if err != nil {
-	// 	log.Println(err)
+	// err1 := updateBirth(engine, "s2", 222)
+	// if err1 != nil {
+	// 	log.Println(err1)
 	// }
 
 	//ex3: insert 100 bản ghi vào user: sau đó viết 1 workerpool scantableuser lấy ra tên của các user inra màn hình (Dùng scan theo row) dùng 2 worker và thiết lập bộ đếm ${counter} - ${id} - ${name}
 
 	// insert100(engine)
+	// delete100(engine)
 	// scantableuser(engine)
 
 }
